d6582fcac8646da3c7f2af271b0479d052cad151
Katie-217 <Katie-217@users.noreply.github.com>
2026-05-19 21:04:17 +0700

    Added Label and TextBox for Course Name

--- WinFormsStudentApp/Form1.Designer.cs
+++ WinFormsStudentApp/Form1.Designer.cs
@@ -28,11 +28,15 @@ partial class Form1
     /// </summary>
     private System.Windows.Forms.Label lblStudentName;
     private System.Windows.Forms.TextBox txtStudentName;
+    private System.Windows.Forms.Label lblCourseName;
+    private System.Windows.Forms.TextBox txtCourseName;
 
     private void InitializeComponent()
     {
         this.lblStudentName = new System.Windows.Forms.Label();
         this.txtStudentName = new System.Windows.Forms.TextBox();
+        this.lblCourseName = new System.Windows.Forms.Label();
+        this.txtCourseName = new System.Windows.Forms.TextBox();
         this.SuspendLayout();
         // 
         // lblStudentName
@@ -51,11 +55,29 @@ partial class Form1
         this.txtStudentName.Size = new System.Drawing.Size(200, 23);
         this.txtStudentName.TabIndex = 1;
         // 
-        // Form1
+        // lblCourseName
         // 
+        this.lblCourseName.AutoSize = true;
+        this.lblCourseName.Location = new System.Drawing.Point(30, 70);
+        this.lblCourseName.Name = "lblCourseName";
+        this.lblCourseName.Size = new System.Drawing.Size(100, 15);
+        this.lblCourseName.TabIndex = 2;
+        this.lblCourseName.Text = "Course Name:";
+        // 
+        // txtCourseName
+        // 
+        this.txtCourseName.Location = new System.Drawing.Point(140, 67);
+        this.txtCourseName.Name = "txtCourseName";
+        this.txtCourseName.Size = new System.Drawing.Size(200, 23);
+        this.txtCourseName.TabIndex = 3;
+        // 
+        // Form1
+        //
         this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Controls.Add(this.txtCourseName);
+        this.Controls.Add(this.lblCourseName);
         this.Controls.Add(this.txtStudentName);
         this.Controls.Add(this.lblStudentName);
         this.Name = "Form1";
