db491c5614f92b2147e8118be64a2d47f1edc2e6
ThienKim2107 <ThienKim2107@users.noreply.github.com>
2026-05-19 21:03:43 +0700

    Added Label and TextBox for Student Name

--- WinFormsStudentApp/Form1.Designer.cs
+++ WinFormsStudentApp/Form1.Designer.cs
@@ -26,12 +26,42 @@ partial class Form1
     ///  Required method for Designer support - do not modify
     ///  the contents of this method with the code editor.
     /// </summary>
+    private System.Windows.Forms.Label lblStudentName;
+    private System.Windows.Forms.TextBox txtStudentName;
+
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
+        this.lblStudentName = new System.Windows.Forms.Label();
+        this.txtStudentName = new System.Windows.Forms.TextBox();
+        this.SuspendLayout();
+        // 
+        // lblStudentName
+        // 
+        this.lblStudentName.AutoSize = true;
+        this.lblStudentName.Location = new System.Drawing.Point(30, 30);
+        this.lblStudentName.Name = "lblStudentName";
+        this.lblStudentName.Size = new System.Drawing.Size(100, 15);
+        this.lblStudentName.TabIndex = 0;
+        this.lblStudentName.Text = "Student Name:";
+        // 
+        // txtStudentName
+        // 
+        this.txtStudentName.Location = new System.Drawing.Point(140, 27);
+        this.txtStudentName.Name = "txtStudentName";
+        this.txtStudentName.Size = new System.Drawing.Size(200, 23);
+        this.txtStudentName.TabIndex = 1;
+        // 
+        // Form1
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        this.Controls.Add(this.txtStudentName);
+        this.Controls.Add(this.lblStudentName);
+        this.Name = "Form1";
+        this.Text = "Student App";
+        this.ResumeLayout(false);
+        this.PerformLayout();
     }
 
     #endregion
